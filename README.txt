edd412c276a1e905965580649ed0ca22dfe2e15a
KittenRoar <yanickvali@gmail.com>
2019-04-30 11:33:47 -0400

    Ajout du fichier README

---
+++ README.txt
@@ -0,0 +1,9 @@
+Auteurs : Yanick Valiquette & Maxime Ouellette
+
+Version : Livrable #2
+
+
+/!\IMPORTANT/!\
+Pour accéder à l'application, vous devez entrez les informations suivantes :
+Nom d'utilisateur : bob
+Mot de passe : 123
---
+++ src/vue/CellsRenderer.java
@@ -0,0 +1,25 @@
+package vue;
+
+import java.awt.Component;
+import java.awt.Font;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+public class CellsRenderer extends DefaultTableCellRenderer{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Component getTableCellRendererComponent( JTable table, Object value, boolean isSelected, boolean hasFocus,
+			int row, int column ) {
+        super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+		setHorizontalAlignment( DefaultTableCellRenderer.CENTER );		
+		setFont(getFont().deriveFont( Font.BOLD )) ;
+		return this;
+	}
+
+}
